c693492118cc96e9dd6ebed3d191d571f9ebef7e
Svyatoslav Matyuha <svyatoslavmatyuha@gmail.com>
2023-07-08 19:54:14 +0300

    chore: update authController

--- authController.js
+++ authController.js
@@ -1,11 +1,35 @@
+const User = require('./models/User')
+const Role = require('./models/Role')
+const bcrypt = require('bcryptjs')
+const { validationResult } = require('express-validator')
 class authController {
   async registration(req, res) {
     try {
-    } catch (e) {}
+      const errors = validationResult(req)
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ message: 'Registration error' })
+      }
+      const { username, password } = req.body
+      const candidate = await User.findOne({ username: username })
+      if (candidate) {
+        return res.status(400).json({ message: 'User already registered' })
+      }
+      const hashPassword = bcrypt.hashSync(password, 7)
+      const userRole = await Role.findOne({ value: 'USER' })
+      const user = new User({ username, password: hashPassword, roles: [userRole.value] })
+      await user.save()
+      return res.json({ message: 'User successfully registered' })
+    } catch (e) {
+      console.log(e)
+      res.status(404).json({ message: 'Registration error' })
+    }
   }
   async login(req, res) {
     try {
-    } catch (e) {}
+    } catch (e) {
+      console.log(e)
+      res.status(404).json({ message: 'Login error' })
+    }
   }
   async getUsers(req, res) {
     try {
--- authRouter.js
+++ authRouter.js
@@ -1,9 +1,16 @@
 const Router = require('express')
 const router = new Router()
 const controller = require('./authController')
+const { check } = require('express-validator')
 
-router.post('/registration',controller.registration)
-router.post('/login',controller.login)
-router.get('/users',controller.getUsers)
+router.post('/registration', controller.registration, [
+  check('username', 'Name cannot be empty').notEmpty(),
+  check('password', 'Password must be more than 4 characters and less than 15').isLength({
+    min: 4,
+    max: 15
+  })
+])
+router.post('/login', controller.login)
+router.get('/users', controller.getUsers)
 
 module.exports = router
