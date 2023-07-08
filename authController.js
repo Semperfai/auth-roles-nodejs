ec5efd2630a135e67e159b38a56ccbbe3ad5d49a
Svyatoslav Matyuha <svyatoslavmatyuha@gmail.com>
2023-07-08 19:14:38 +0300

    fix: paths

--- authController.js
+++ authController.js
@@ -1,20 +1,17 @@
 class authController {
-   async registration(req,res) {
-    try{
-
-    } catch(e){}
-   }
-   async login(req,res) {
-    try{
-
-    } catch(e){}
-   }
-   async getUsers(req,res) {
-    try{
-      res.json("server work")
-    } catch(e){}
-   }
-
+  async registration(req, res) {
+    try {
+    } catch (e) {}
+  }
+  async login(req, res) {
+    try {
+    } catch (e) {}
+  }
+  async getUsers(req, res) {
+    try {
+      res.json('server work')
+    } catch (e) {}
+  }
 }
 
-module.exports = new authController();
+module.exports = new authController()
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express');
+const express = require('express')
+const mongoose = require('mongoose')
+const authRouter = require('./authRouter')
 const PORT = process.env.PORT || 5000
-const mongoose = require('mongoose');
-const authRouter = require('./authRouter,js')
+
 const app = express()
+
 app.use(express.json())
-app.use('auth',authRouter)
+app.use('/auth', authRouter)
 
 const start = async () => {
-    try {
-        await mongoose.connect(`mongodb+srv://svyat:Test123@cluster0.p2pami3.mongodb.net/`)
-        app.listen(PORT,() => console.log(`listening on port ${PORT}`))
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    await mongoose.connect(`mongodb+srv://svyat:Test123@cluster0.p2pami3.mongodb.net/`)
+    app.listen(PORT, () => console.log(`listening on port ${PORT}`))
+  } catch (e) {
+    console.log(e)
+  }
 }
 
 start()
